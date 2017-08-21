30079c2e23b026db418a99431ac6bec5d5f3cd5f
Brian Clark <brian@clarkio.com>
2017-08-21 12:33:20 -0400

    reset

--- src/maskEmail.js
+++
@@ -1,7 +0,0 @@
-hideEmail();
-
-function hideEmail() {
-    let emailLabel = document.getElementsByClassName('fxs-avatarmenu-tenant')[0];
-    console.log(emailLabel);
-    emailLabel.style.display = 'none';
-}
--- src/unmaskEmail.js
+++
@@ -1,6 +0,0 @@
-unhideEmail();
-
-function unhideEmail() {
-    let emailLabel = document.getElementsByClassName('fxs-avatarmenu-tenant')[0];
-    emailLabel.style.display = null;
-}
