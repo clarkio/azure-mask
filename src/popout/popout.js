f6e3e37e6df2ccf414e969dc8b44fee0c32f7f5a
Brian Clark <clarkio@users.noreply.github.com>
2017-08-21 18:22:03 -0400

    Popout (#2)
    * init
    
    * add html
    
    * add popout css and js
    
    * merge in master
    
    * add toggle in popout
    
    * simplify toggle

---
+++ src/popout/popout.js
@@ -0,0 +1,24 @@
+const sensitiveDataRegex = /^([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})|((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))$/;
+const sensitiveDataClassName = 'azdev-sensitive';
+let allMasksEnabled = true;
+
+let allMasksCheckbox = document.getElementById('toggle-all-masks');
+allMasksCheckbox.addEventListener('click', toggleAllMasks);
+
+function toggleAllMasks() {
+  console.log('Toggling...');
+  allMasksEnabled = !allMasksEnabled;
+  allMasksEnabled ? injectEnableAllMasks() : injectDisableAllMasks();
+}
+
+function injectEnableAllMasks() {
+  chrome.tabs.executeScript({
+    code: 'document.body.classList.add(\'az-mask-enabled\');'
+  });
+}
+
+function injectDisableAllMasks() {
+  chrome.tabs.executeScript({
+    code: 'document.body.classList.remove(\'az-mask-enabled\');'
+  });
+}
