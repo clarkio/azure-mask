e5b91065cdd23a2673cacc318d3dca2a66a2d553
Anthony Chu <anthony@anthonychu.ca>
2017-08-24 08:52:28 -0700

    Mask keys and connection strings and fix perf issues (#7)
    * Blur out input boxes for keys, conn strings, etc
    
    * Unhide "report a bug" when masking is off
    
    * Update readme
    
    * Comment CSS rules
    
    * MORE FASTER!
    
    * MORE FASTER (correctly)

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Azure Mask
-This is a Chrome extension that will mask GUIDs (such as Subscription IDs) and email addresses with a blur. The intention of the extension is to make it easier to do screen recordings without revealing sensitive, personal, account information that may show up on screen. It will only run and apply against Azure portal URLs.
+This is a Chrome extension that will mask GUIDs (such as Subscription IDs), email addresses, keys, and connection strings with a blur. The intention of the extension is to make it easier to do screen recordings without revealing sensitive, personal, account information that may show up on screen. It will only run and apply against Azure portal URLs.
 
 ![screen shot](azure-mask-screen-shot.png)
 
 ## Features
 
 * Blurs GUIDs (such as Subscription IDs)
-* Blurs your accout email
+* Blurs your account email
 * Hides the "Report a Bug" button (if found)
 * Toggle the mask on/off and store this state
 * Apply the mask (if enabled) after Document Object Model (DOM) mutations
--- src/content-script/mask-process.js
+++ src/content-script/mask-process.js
@@ -2,13 +2,17 @@ const isMaskedKeyName = 'isMasked';
 const maskEnabledClassName = 'az-mask-enabled';
 const sensitiveDataRegex = /^\s*([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})|((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))\s*$/;
 const sensitiveDataClassName = 'azdev-sensitive';
+const blurCss = 'filter: blur(5px);';
+const tagNamesToMatch = ['DIV']; // uppercase
 
 // add CSS style to blur
 const style = document.createElement('style');
 style.appendChild(document.createTextNode(''));
 document.head.appendChild(style);
-style.sheet.insertRule(`.${maskEnabledClassName} .azdev-sensitive { filter: blur(5px); }`);
-style.sheet.insertRule("a.fxs-topbar-reportbug { display:none; }");
+
+style.sheet.insertRule(`.${maskEnabledClassName} .azdev-sensitive { ${blurCss} }`);
+style.sheet.insertRule(`.${maskEnabledClassName} input.azc-bg-light { ${blurCss} }`); // input boxes used for keys, connection strings, etc
+style.sheet.insertRule(`.${maskEnabledClassName} a.fxs-topbar-reportbug { display:none; }`); // report a bug button (MS internal only)
 
 getStoredMaskedStatus(isMasked => {
   isMasked ? document.body.classList.add(maskEnabledClassName) : document.body.classList.remove(maskEnabledClassName);
@@ -16,14 +20,14 @@ getStoredMaskedStatus(isMasked => {
 
 // add class to elements already on the screen
 Array
-  .from(document.getElementsByTagName('*'))
-  .filter(e => sensitiveDataRegex.test(e.textContent))
+  .from(document.querySelectorAll(tagNamesToMatch.join()))
+  .filter(e => shouldCheckContent(e) && sensitiveDataRegex.test(e.textContent))
   .forEach(e => e.classList.add(sensitiveDataClassName));
 
 // add class to elements that are added to DOM later
 const observer = new MutationObserver(mutations => {
   mutations
-    .filter(m => sensitiveDataRegex.test(m.target.textContent))
+    .filter(m => shouldCheckContent(m.target) && sensitiveDataRegex.test(m.target.textContent))
     .forEach(m => {
       const node = m.type === 'characterData' ? m.target.parentNode : m.target;
       if (node.classList) {
@@ -39,12 +43,16 @@ const config = {
 };
 observer.observe(document.body, config);
 
+function shouldCheckContent(target) {
+  return target
+    && tagNamesToMatch.some(tn => tn === target.tagName);
+}
 
 function getStoredMaskedStatus(callback) {
   chrome.storage.local.get(isMaskedKeyName, items => {
-    const {isMasked} = items;
-    // default to true
+    const { isMasked } = items;
     if (typeof isMasked !== 'boolean') {
+      // default to true
       chrome.storage.local.set({ [isMaskedKeyName]: true }, () => callback(true));
     } else {
       callback(isMasked);
