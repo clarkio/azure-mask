fda2d3bc54bbe77970e452e9221c58715eff2c2f
Brian Clark <clarkio@users.noreply.github.com>
2019-04-22 16:18:56 -0400

    0.1.5 (#53)
    * wip
    
    * remove unnecessary code in popout
    
    * wrap up 1.1.5

--- src/content-script/mask-process.js
+++ src/content-script/mask-process.js
@@ -1,8 +1,12 @@
 const isMaskedKeyName = 'isMasked';
 const maskEnabledClassName = 'az-mask-enabled';
-const sensitiveDataRegex = /^\s*([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})|((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))\s*$/;
+const sensitiveDataRegex = /^([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})$/;
+/* ** Original regex prior to 2019-04-18 **
+ * const sensitiveDataRegex = /^\s*([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})|((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))\s*$/;
+ *
+ */
 const sensitiveDataClassName = 'azdev-sensitive';
-const blurCss = 'filter: blur(10px);';
+const blurCss = 'filter: blur(10px); pointer-events: none;';
 const tagNamesToMatch = ['DIV']; // uppercase
 
 // add CSS style to blur
@@ -10,13 +14,29 @@ const style = document.createElement('style');
 style.appendChild(document.createTextNode(''));
 document.head.appendChild(style);
 
-style.sheet.insertRule(`.${maskEnabledClassName} .azdev-sensitive { ${blurCss} }`);
-style.sheet.insertRule(`.${maskEnabledClassName} .fxs-avatarmenu-username { display: none }`); // hide name instead of blurring
-style.sheet.insertRule(`.${maskEnabledClassName} input.azc-bg-light { ${blurCss} }`); // input boxes used for keys, connection strings, etc
-style.sheet.insertRule(`.${maskEnabledClassName} a.fxs-topbar-reportbug { display:none; }`); // report a bug button (MS internal only)
+style.sheet.insertRule(
+  `.${maskEnabledClassName} .azdev-sensitive { ${blurCss} }`
+);
+style.sheet.insertRule(
+  `.${maskEnabledClassName} .fxs-avatarmenu-username { display: none }`
+); // hide name instead of blurring
+style.sheet.insertRule(
+  `.${maskEnabledClassName} input.azc-bg-light { ${blurCss} }`
+); // input boxes used for keys, connection strings, etc
+style.sheet.insertRule(
+  `.${maskEnabledClassName} a.fxs-topbar-reportbug { display:none; }`
+); // report a bug button (MS internal only)
+style.sheet.insertRule(
+  `.${maskEnabledClassName} div.fxs-topbar-internal { display:none; }`
+); // "Preview" element in top navigation bar (MS internal only)
+style.sheet.insertRule(
+  `.${maskEnabledClassName} .fxs-mecontrol-flyout { ${blurCss} }`
+); // user account menu
 
 getStoredMaskedStatus(isMasked => {
-  isMasked ? document.body.classList.add(maskEnabledClassName) : document.body.classList.remove(maskEnabledClassName);
+  isMasked
+    ? document.body.classList.add(maskEnabledClassName)
+    : document.body.classList.remove(maskEnabledClassName);
 });
 
 // add class to elements already on the screen
@@ -27,7 +47,11 @@ Array.from(document.querySelectorAll(tagNamesToMatch.join()))
 // add class to elements that are added to DOM later
 const observer = new MutationObserver(mutations => {
   mutations
-    .filter(m => shouldCheckContent(m.target, m.type) && sensitiveDataRegex.test(m.target.textContent))
+    .filter(
+      m =>
+        shouldCheckContent(m.target, m.type) &&
+        sensitiveDataRegex.test(m.target.textContent.trim())
+    )
     .forEach(m => {
       const node = m.type === 'characterData' ? m.target.parentNode : m.target;
       if (node.classList) {
@@ -44,7 +68,10 @@ const config = {
 observer.observe(document.body, config);
 
 function shouldCheckContent(target, mutationType) {
-  return mutationType === 'characterData' || (target && tagNamesToMatch.some(tn => tn === target.tagName));
+  return (
+    mutationType === 'characterData' ||
+    (target && tagNamesToMatch.some(tn => tn === target.tagName))
+  );
 }
 
 function getStoredMaskedStatus(callback) {
@@ -52,7 +79,9 @@ function getStoredMaskedStatus(callback) {
     const { isMasked } = items;
     if (typeof isMasked !== 'boolean') {
       // default to true
-      chrome.storage.local.set({ [isMaskedKeyName]: true }, () => callback(true));
+      chrome.storage.local.set({ [isMaskedKeyName]: true }, () =>
+        callback(true)
+      );
     } else {
       callback(isMasked);
     }
--- src/popout/popout.js
+++ src/popout/popout.js
@@ -1,5 +1,3 @@
-const sensitiveDataRegex = /^([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})|((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))$/;
-const sensitiveDataClassName = 'azdev-sensitive';
 let allMasksEnabled = true;
 
 let allMasksCheckbox = document.getElementById('toggle-all-masks');
@@ -8,7 +6,7 @@ allMasksCheckbox.addEventListener('click', toggleAllMasks);
 chrome.tabs.executeScript(
   {
     code: "document.body.classList.contains('az-mask-enabled');",
-    allFrames: false
+    allFrames: true
   },
   results => {
     if (results) {
@@ -39,11 +37,11 @@ function injectDisableAllMasks() {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var y = document.getElementById("index_link");
-  y.addEventListener("click", openIndex);
+document.addEventListener('DOMContentLoaded', function() {
+  var y = document.getElementById('index_link');
+  y.addEventListener('click', openIndex);
 });
 
 function openIndex() {
-  chrome.tabs.create({ active: true, url: "https://aka.ms/publicportal" });
+  chrome.tabs.create({ active: true, url: 'https://aka.ms/publicportal' });
 }
