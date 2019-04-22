0b0ae31bb223d80469857c1d3e7b7ab0479e346c
clarkio <clarkio@users.noreply.github.com>
2019-04-22 14:24:22 -0400

    remove unnecessary code in popout

--- src/popout/popout.js
+++ src/popout/popout.js
@@ -1,5 +1,3 @@
-const sensitiveDataRegex = /^([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})|((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))$/;
-const sensitiveDataClassName = 'azdev-sensitive';
 let allMasksEnabled = true;
 
 let allMasksCheckbox = document.getElementById('toggle-all-masks');
